9d6edd93b1fd248b302a1cd2c5709a3631192b16
sdcew <brevkarl1@gmail.com>
2026-05-15 18:17:19 +0700

    сделал картинку для мейна

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,33 @@
 
 <template>
 
-  <div>
-    <h2>Крутой текст</h2>
-    <div>
-      <button>Кнопка которая ничего не делает</button>
+  <div class="main">
+    <div class="hero">
+    <img src="../images/mainHome/Hero.png" width="1910"/>
     </div>
+
+
+
+  <div>
+
   </div>
 
+
+  <div>
+
+  </div>
+
+  </div>
 
 </template>
 
 <style scoped>
-button {
-  color: #f2f2f2;
-  background: black;
+img {
+  max-width: 100%; /* Картинка не вылезает за пределы контейнера */
+  height: auto;    /* Высота меняется автоматически */
+}
+
+template{
+  margin: 0 auto;
 }
 </style>
